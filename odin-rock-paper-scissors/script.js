fc8e88e8cf64ea5dc66af9f2eb3d91e209aa522c
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-02-25 23:10:46 -0500

    Add prompt to get user's choice; assign variables to results of bot and user choice; play one round of game

--- odin-rock-paper-scissors/script.js
+++ odin-rock-paper-scissors/script.js
@@ -1,4 +1,23 @@
+let computerChoice = getComputerChoice();
+let playerChoice = getPlayerChoice();
+
 function getComputerChoice() {
-  let computerChoice = ["Rock", "Paper", "Scissors"];
-  return computerChoice[Math.floor(Math.random() * 3)];
+  let computerOptions = ["rock", "paper", "scissors"];
+  return computerOptions[Math.floor(Math.random() * 3)];
+}
+
+function getPlayerChoice() {
+  return prompt("Please enter 'Rock', 'Paper', or 'Scissors'").toLowerCase();
+}
+
+function playRound(computerChoice, playerChoice) {
+  return computerChoice == playerChoice
+    ? "Tie."
+    : computerChoice == "rock" && playerChoice == "scissors"
+    ? "Computer wins. Rock beats Scissors"
+    : computerChoice == "paper" && playerChoice == "rock"
+    ? "Computer wins. Paper beats Rock"
+    : computerChoice == "scissors" && playerChoice == "paper"
+    ? "Computer wins. Scissors beats Paper"
+    : "You win.";
 }
