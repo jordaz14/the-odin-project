5220f30f3f6a5af0d8583ddd7cc60414f8d260ad
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-03-02 15:18:50 -0500

    Create 16 x 16 grid of ContainerSquares using 2 for loops

--- odin-etch-a-sketch/script.js
+++ odin-etch-a-sketch/script.js
@@ -1,8 +1,12 @@
 const container = document.querySelector(".container");
 
 for (i = 0; i < 16; i++) {
-  const containerSquare = document.createElement("div");
-  containerSquare.classList.add("container-square");
-
-  container.appendChild(containerSquare);
+  const containerRow = document.createElement("div");
+  containerRow.classList.add("container-row");
+  for (j = 0; j < 16; j++) {
+    const containerSquare = document.createElement("div");
+    containerSquare.classList.add("container-square");
+    containerRow.appendChild(containerSquare);
+  }
+  container.appendChild(containerRow);
 }
