8ebd09c2c4badedf962efbf1606bed1600b9e42e
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-03-02 16:07:39 -0500

    Add event listener to resetButton, prompting user for grid size, clearing existing grid, and re-outputting per user's input

--- odin-etch-a-sketch/script.js
+++ odin-etch-a-sketch/script.js
@@ -1,11 +1,21 @@
 const container = document.querySelector(".container");
 const resetButton = document.querySelector("#resetButton");
 
-function createGrid(dimension) {
-  for (i = 0; i < dimension; i++) {
+resetButton.addEventListener("click", () => {
+  let gridSize = prompt("Please enter grid size (e.g. X by X grids): ");
+  if (gridSize < 16 || gridSize > 100) {
+    alert("Invalid grid size. Please try again");
+  } else {
+    container.innerHTML = "";
+    createGrid(gridSize);
+  }
+});
+
+function createGrid(gridSize) {
+  for (i = 0; i < gridSize; i++) {
     const containerRow = document.createElement("div");
     containerRow.classList.add("container-row");
-    for (j = 0; j < dimension; j++) {
+    for (j = 0; j < gridSize; j++) {
       const containerSquare = document.createElement("div");
       containerSquare.classList.add("container-square");
       containerSquare.addEventListener("mouseover", () => {
