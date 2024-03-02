4f4caf28a9376b6d95d935413d9720c258e45b68
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-03-02 12:36:29 -0500

    Refactor game to gather user input from buttons & first to 5 wins

--- odin-rock-paper-scissors/script.js
+++ odin-rock-paper-scissors/script.js
@@ -1,11 +1,39 @@
+let computerWins = 0;
+let playerWins = 0;
+let gameTies = 0;
+
 function getComputerChoice() {
   let computerOptions = ["rock", "paper", "scissors"];
   return computerOptions[Math.floor(Math.random() * 3)];
 }
 
-function getPlayerChoice() {
-  return prompt("Please enter 'Rock', 'Paper', or 'Scissors'").toLowerCase();
-}
+const rockButton = document.querySelector("#rockButton");
+const paperButton = document.querySelector("#paperButton");
+const scissorsButton = document.querySelector("#scissorsButton");
+
+const roundStatus = document.querySelector("#roundStatus");
+const gameStatus = document.querySelector("#roundStatus");
+
+rockButton.addEventListener("click", () => {
+  playerChoice = rockButton.textContent;
+  let result = playRound(getComputerChoice(), playerChoice);
+  tallyResults(result);
+  checkEndGame(result);
+});
+
+paperButton.addEventListener("click", () => {
+  playerChoice = paperButton.textContent;
+  let result = playRound(getComputerChoice(), playerChoice);
+  tallyResults(result);
+  checkEndGame(result);
+});
+
+scissorsButton.addEventListener("click", () => {
+  playerChoice = scissorsButton.textContent;
+  let result = playRound(getComputerChoice(), playerChoice);
+  tallyResults(result);
+  checkEndGame(result);
+});
 
 function playRound(computerChoice, playerChoice) {
   return computerChoice == playerChoice
@@ -19,23 +47,20 @@ function playRound(computerChoice, playerChoice) {
     : `You win. ${playerChoice} beats ${computerChoice}`;
 }
 
-function playGame() {
-  let computerWins = 0;
-  let playerWins = 0;
-  let gameTies = 0;
-  for (let i = 0; i < 5; i++) {
-    let result = playRound(getComputerChoice(), getPlayerChoice());
-    result == "Tie."
-      ? gameTies++
-      : result.includes("You win.")
-      ? playerWins++
-      : computerWins++;
-
-    console.log(
-      `Ties: ${gameTies}, Computer: ${computerWins}, Player: ${playerWins}`
-    );
-  }
-  console.log("Thanks for playing!");
+function tallyResults(result) {
+  result == "Tie."
+    ? gameTies++
+    : result.includes("You win.")
+    ? playerWins++
+    : computerWins++;
+  roundStatus.innerText = `${result} \n Ties: ${gameTies}, Computer: ${computerWins}, Player: ${playerWins}`;
 }
 
-playGame();
+function checkEndGame(result) {
+  if (computerWins == 5 || playerWins == 5) {
+    roundStatus.innerText = `${result} \n Ties: ${gameTies}, Computer: ${computerWins}, Player: ${playerWins} \n GAME OVER. Scores will now reset.`;
+    computerWins = 0;
+    playerWins = 0;
+    gameTies = 0;
+  }
+}
