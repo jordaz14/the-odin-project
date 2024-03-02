1e40c2248f2c3ac37b9bf80cf88f9f1710e9989f
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-03-02 16:11:41 -0500

    script.js

--- odin-etch-a-sketch/script.js
+++ odin-etch-a-sketch/script.js
@@ -1,8 +1,11 @@
+// Query for DOM elements
 const container = document.querySelector(".container");
 const resetButton = document.querySelector("#resetButton");
 
+// Click reset button to prompt for user input on grid size
 resetButton.addEventListener("click", () => {
   let gridSize = prompt("Please enter grid size (e.g. X by X grids): ");
+  // Deny request if grid size too small or large
   if (gridSize < 16 || gridSize > 100) {
     alert("Invalid grid size. Please try again");
   } else {
@@ -11,10 +14,13 @@ resetButton.addEventListener("click", () => {
   }
 });
 
+// Creates an X by X grid per gridSize parameter
 function createGrid(gridSize) {
+  // Creates X rows
   for (i = 0; i < gridSize; i++) {
     const containerRow = document.createElement("div");
     containerRow.classList.add("container-row");
+    // Creates X squares in row
     for (j = 0; j < gridSize; j++) {
       const containerSquare = document.createElement("div");
       containerSquare.classList.add("container-square");
@@ -27,4 +33,5 @@ function createGrid(gridSize) {
   }
 }
 
+// Run initial 16x16 grid size
 createGrid(16);
