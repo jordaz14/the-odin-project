2fb4823f4a1d4717185bfe631048b243418966c7
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-03-04 20:33:26 -0500

    Create calcNums function, taking an operator and two numbers to performance a simple arithemtic calculation

--- odin-calculator/script.js
+++ odin-calculator/script.js
@@ -15,3 +15,19 @@ function multiplyNums(n1, n2) {
 function divideNums(n1, n2) {
   return n1 / n2;
 }
+
+let firstNum;
+let secondNum;
+let operator;
+
+function calcNums(operator, firstNum, secondNum) {
+  return operator == "+"
+    ? addNums(firstNum, secondNum)
+    : operator == "-"
+    ? subtractNums(firstNum, secondNum)
+    : operator == "*"
+    ? multiplyNums(firstNum, secondNum)
+    : operator == "/"
+    ? divideNums(firstNum, secondNum)
+    : "ERR";
+}
