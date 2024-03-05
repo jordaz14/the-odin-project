d98d97c7c94a99936ed69e14b3c2d1a732afcdb3
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-03-04 20:26:04 -0500

    Create add, subtract, multiply, and divide functions

--- odin-calculator/script.js
+++ odin-calculator/script.js
@@ -1,1 +1,17 @@
 console.log("Hello World");
+
+function addNums(n1, n2) {
+  return n1 + n2;
+}
+
+function subtractNums(n1, n2) {
+  return n1 - n2;
+}
+
+function multiplyNums(n1, n2) {
+  return n1 * n2;
+}
+
+function divideNums(n1, n2) {
+  return n1 / n2;
+}
