f63d1e785fe69a07291a9017c3eb8ff784b531f7
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-06-11 21:11:14 -0400

    Add basic functionality to add book form submissions as objects to myLibray array

--- odin-library/script.js
+++ odin-library/script.js
@@ -1,9 +1,31 @@
 const myLibrary = [];
 
-function Book() {
+const button = document.querySelector(".submit-button");
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  addBookToLibrary();
+});
+
+function Book(title, author, pages, read) {
   // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
   // do stuff here
+  console.log("hi");
+
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let pages = document.querySelector("#pages").value;
+
+  console.log(title, author, pages);
+
+  let newBook = new Book(title, author, pages, false);
+  console.log(newBook);
+
+  myLibrary.push(newBook);
 }
