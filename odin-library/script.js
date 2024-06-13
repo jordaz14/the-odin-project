8b80efc9b9c8422d9161805d98b2b5c0f5e31b8c
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-06-12 20:05:44 -0400

    Add mark as read functionality to button

--- odin-library/script.js
+++ odin-library/script.js
@@ -6,6 +6,8 @@ button.addEventListener("click", (e) => {
   addBookToLibrary();
 });
 
+const bookSection = document.querySelector("section");
+
 function Book(title, author, pages, read) {
   // the constructor...
   this.title = title;
@@ -16,16 +18,55 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary() {
   // do stuff here
-  console.log("hi");
+
+  // clears catalog of books
+  bookSection.innerHTML = "";
 
   let title = document.querySelector("#title").value;
   let author = document.querySelector("#author").value;
   let pages = document.querySelector("#pages").value;
 
-  console.log(title, author, pages);
-
-  let newBook = new Book(title, author, pages, false);
-  console.log(newBook);
+  let newBook = new Book(title, author, pages, "Unread");
 
   myLibrary.push(newBook);
+
+  // iterates through books cards and posts to DOM
+  for (let book of myLibrary) {
+    console.log(book.title);
+    const bookCard = document.createElement("div");
+    bookCard.className = "book-card";
+    bookSection.appendChild(bookCard);
+
+    const bookTitle = document.createElement("h2");
+    bookTitle.textContent = book.title;
+
+    const bookAuthor = document.createElement("p");
+    bookAuthor.textContent = book.author;
+
+    const bookRead = document.createElement("p");
+    bookRead.textContent = book.read;
+
+    const bookPages = document.createElement("p");
+    bookPages.textContent = `${book.pages} pages`;
+
+    const bookReadButton = document.createElement("button");
+    bookReadButton.className = "read-button";
+    bookReadButton.textContent = "Mark as Read";
+    bookReadButton.addEventListener("click", () => {
+      bookRead.textContent = "Read";
+    });
+
+    const bookDeleteButton = document.createElement("button");
+    bookDeleteButton.className = "delete-button";
+    bookDeleteButton.textContent = "Delete";
+
+    bookCard.append(
+      bookTitle,
+      bookAuthor,
+      bookPages,
+      bookRead,
+      bookReadButton,
+      bookDeleteButton
+    );
+  }
 }
